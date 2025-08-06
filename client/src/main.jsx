8e7db16cc191c1afc4d9187c0568f065bd74c75a
rawaha <rawahaahmed081@gmail.com>
2025-08-06 18:35:35 +0500

    'commit'

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ToastContainer, toast } from 'react-toastify';
+
 
 ;
 
@@ -15,19 +15,7 @@ createRoot(document.getElementById('root')).render(
 
     <App />
      
-    <ToastContainer
-position="top-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick={false}
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="dark"
 
-/>
 
 
   </StrictMode>,
