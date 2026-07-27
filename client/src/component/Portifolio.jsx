2b9adc70e822a234abfcbbb24d01f954b6263bb1
Rawahaahmed23 <rawahaahmed081@gmail.com>
2026-07-28 04:42:36 +0500

    commit

--- client/src/component/Portifolio.jsx
+++ client/src/component/Portifolio.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { Github, ExternalLink } from "lucide-react"; // import icons
+import { Github, ExternalLink } from "lucide-react"; 
 
 const projects = [
 
